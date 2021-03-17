6164e7b73f191958810017a55d24235d09186bfa
dg1886 <dgarbuz1888@gmail.com>
2021-03-17 14:38:44 +0300

    create api.tsx with Data Access Layer (with get users api); refactoring UesersContainet;

--- src/Components/Users/UsersContainer.tsx
+++ src/Components/Users/UsersContainer.tsx
@@ -11,6 +11,7 @@ import {UsersType} from "../../redux/store";
 import axios from "axios";
 import Users from "./Users";
 import Preloader from "../common/Preloader/Preloader";
+import {usersAPI} from "../../api/api";
 
 export type UsersContainerPropsType = {
     users: Array<UsersType>
@@ -29,23 +30,22 @@ export type UsersContainerPropsType = {
 
 class UsersContainer extends React.Component<UsersContainerPropsType> {
     componentDidMount() {
-        this.props.setToggleIsFetching(true)
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`,
-            {withCredentials: true}
-            )
-            .then(response => {
+
+        usersAPI.getUsers(this.props.currentPage, this.props.pageSize).then(data => {
                 this.props.setToggleIsFetching(false)
-                this.props.setUsers(response.data.items)
-                this.props.setUsersTotalCount(response.data.totalCount)
+                this.props.setUsers(data.items)
+                this.props.setUsersTotalCount(data.totalCount)
             })
     }
     onPageChanged=(pageNumber:number)=>{
         this.props.setCurrentPage(pageNumber)
         this.props.setToggleIsFetching(true)
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`, {withCredentials: true})
-            .then(response => {
+
+        usersAPI.getUsers(pageNumber, this.props.pageSize)
+        // axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`, {withCredentials: true})
+            .then(data => {
                 this.props.setToggleIsFetching(false)
-                this.props.setUsers(response.data.items)
+                this.props.setUsers(data.items)
             })
     }
 
---
+++ src/api/api.tsx
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const instance = axios.create({
+    withCredentials: true,
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    headers: {'API-KEY': '86f9a91e-2262-457f-972b-f0bb39472774'}
+
+})
+
+export const usersAPI = {
+    getUsers(currentPage: number, pageSize: number) {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`)
+            .then(responce => {
+                return responce.data
+            })
+    }
+}
+
+
